33f70a76593512bf11a624eaf15d889d3954a060
Stormchad <111141991+Stormchad@users.noreply.github.com>
2022-09-28 07:57:29 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+import App from './App'
+
+
+// this will render the App function in app.js in the root div in public
+ReactDOM.render(<App/>, document.getElementById('root'))
